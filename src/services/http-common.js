3b81ea85aa946a33610873d161872e206fb00e91
Cipto <ciptowidarto@gmail.com>
2022-12-07 21:19:44 +0700

    update http

--- src/services/http-common.js
+++ src/services/http-common.js
@@ -1,15 +1,6 @@
 import axios from "axios";
 
-const storage = localStorage.length
-let header = {}
-
-header["Content-type"] = "application/json"
-if (storage > 0) {
-  const token = localStorage.getItem('AUTH_KEY').token
-  header.Authorization = token
-}
-
 export default axios.create({
   baseURL: "https://serverless-890.netlify.app",
-  headers: header
+  "Content-type": "application/json"
 });
