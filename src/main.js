6e175caa9bf4847f349b4e369ba4b6594a7f7a29
Cipto <ciptowidarto@gmail.com>
2022-12-09 01:30:32 +0700

    insert category with token

--- src/main.js
+++ src/main.js
@@ -17,14 +17,14 @@ import "./assets/main.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
-
+const pinia = createPinia()
 const app = createApp(App);
 
 app.component('default-layout', Guest)
 app.component('admin-layout', Admin)
 app.use(BootstrapVue3);
-app.use(createPinia());
 app.use(router);
+app.use(pinia)
 
 app.mount("#app");
 
